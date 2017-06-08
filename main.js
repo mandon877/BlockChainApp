f1d27bbdb8db8348f2fd1b0f3fb808f53ea9cfbb
Jung, Joo-Hea <mandon877@gmail.com>
2017-06-08 06:03:43 +0000

    complete login work

--- main.js
+++ main.js
@@ -5,52 +5,25 @@ var io   = require('socket.io'),
     http = require('http'),
     path = require('path'),
     bodyParser = require('body-parser');
-    
 var app = express();
 var server = http.createServer(app);
 var socket = io.listen(server);
 var session = require('express-session');
-
-var redis = require("redis");
-var redisStore = require('connect-redis')(session);
-var client = redis.createClient();
-
 app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.engine('html', require('ejs').renderFile);
 /////////////////////////////////////////////////////////////////////////////
 // login Start                                                             //
 /////////////////////////////////////////////////////////////////////////////
 app.use(session({
-//  secret: 'aasdfsadf',
-//  resave: true,
-//  saveUninitialized: true,
-//  cookie: {
-//      maxAge: 24 * 60 * 60 * 1000,
-//      secure: true
-//  }
- secret: '@#@$MYSIGN#@$#$',
- store: new redisStore({
-                      //host: 'localhost', 
-                      host: process.env.IP,
-                      port: 6379, 
-                      client: client, 
-                      ttl : 2606
-            }),
- saveUninitialized: true,
- resave: false
-//  cookie: { secure: true}
-    // secret: 'secret',
-    // saveUninitialized: false,
-    // resave: true,
-    //  store: new RedisStore (
-    //      {url: 'redis://localhost' })
+   secret: '@#@$MYSIGN#@$#$',
+   saveUninitialized: false,
+   resave: true
 }));
 app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 function simulatedDB() {
     const data = {
@@ -79,25 +52,25 @@ function simulatedDB() {
 
 var Models = new simulatedDB();
 
-app.use((req, _, next) => {
-  console.log(JSON.stringify(req.session, null, 2))
-  const user = req.session && req.session.user && req.session.user.username || 'Unauthenticated';
-  console.log("  0. " + req.method, req.path, "<",user,">") ;
-  next();
-});
+ app.use((req, _, next) => {
+//   const user = req.session && req.session.user && req.session.user.username || 'Unauthenticated';
+   const user = req.session.key || 'Unauthenticated';
+   console.log(req.method, req.path, "<",user,">") ;
+   next();
+ });
 
 app.post('/login', function(req, res, next) {
     const body = req.body;
-    console.log('  1. Login request received:', body);
+    console.log('Login request received:', body);
     return Models.User
       .login(req.body.username, req.body.password)
       .then(user => {
-        req.session.user = user;
-        console.log("  4. Logged in as: ", user.username);
-        console.log("     req.session: ", req.session);
+        req.session.key=user.username;
+        console.log("Logged in as username : ", req.session.key);
+        //console.log(JSON.stringify(req, null, 2))
         //return res.send({ok:'ok'})
         //res.end('done')
-        res.redirect('/')
+        res.redirect('/');
       })
       .catch(err => {
           console.log(err);
@@ -105,165 +78,134 @@ app.post('/login', function(req, res, next) {
       });
 })
 
-// app.get('/login/:loggedIn', function(req, res){
-//     loggedIn = req.params.loggedIn;
-//     if (loggedIn) res.redirect('/');
-// });
-
-
 app.get('/logout', function(req, res){
-    var sess = req.session;
-    console.log("100. sess.username : " + sess.username);
-    if(sess.username){
-        req.session.destroy(function(err){
-            if(err){
-                console.log(err);
-            }else{
-                res.redirect('/login');
-            }
-        })
-    }else{
-        res.redirect('/login');
-    }
+    req.session.destroy(function(err){
+        if(err)
+            console.log(err);
+        else
+            res.redirect('/login');
+    });
 });
 
 ///////////////////////////////////////////////////////////////////////////////
 // login End                                                                 //
 ///////////////////////////////////////////////////////////////////////////////
-
-app.get('/', function(req, res){
-    //console.log(">> Home(/) req.session.user : " + req.session.user);
-
-    // if (req.session.user)
-    //     res.render('home'); 
-    // else
-    //     res.render('login'); 
-    res.render('home'); 
-});
-
 app.get('/login', function(req, res){
     res.render('login'); 
 });
 
+app.get('/', function(req, res){
+    console.log(">> Home(/) req.session.key : " + req.session.key);
+
+    if (req.session.key)
+        res.render('home');
+    else 
+        res.render('login'); 
+});
+
 app.get('/Simple', function(req, res){
-    // if (loggedIn)
-    //     res.render('SimpleStorage'); 
-    // else
-    //     res.render('login'); 
-    res.render('SimpleStorage'); 
+    if (req.session.key)
+        res.render('SimpleStorage'); 
+    else
+        res.render('login'); 
 });
 
 app.get('/Car', function(req, res){
-    // if (loggedIn)
-    //      res.render('redirectCar'); 
-    // else
-    //     res.render('login');
-    res.render('redirectCar'); 
+    if (req.session.key)
+         res.render('redirectCar'); 
+    else
+        res.render('login');
 });
 
 app.get('/CodePen', function(req, res){
-    // if (loggedIn)
-    //     res.render('CodePenUrl'); 
-    // else
-    //     res.render('login'); 
-    res.render('CodePenUrl'); 
+    if (req.session.key)
+        res.render('CodePenUrl'); 
+    else
+        res.render('login'); 
 });
 
 app.get('/Angular', function(req, res){
-    // if (loggedIn)
-    //     res.render('AngularJsTest'); 
-    // else
-    //     res.render('login');
-    res.render('AngularJsTest'); 
+    if (req.session.key)
+        res.render('AngularJsTest'); 
+    else
+        res.render('login');
 });
 
 app.get('/MiningMechine', function(req, res){
-    // if (loggedIn)
-    //     res.render('MiningMechine'); 
-    // else
-    //     res.render('login');
-    res.render('MiningMechine'); 
+    if (req.session.key)
+        res.render('MiningMechine'); 
+    else
+        res.render('login');
 });
 
 app.get('/Ethereum', function(req, res){
-    // if (loggedIn)
-    //     res.render('EthereumGit'); 
-    // else
-    //     res.render('login');
-    res.render('EthereumGit'); 
+    if (req.session.key)
+        res.render('EthereumGit'); 
+    else
+        res.render('login');
 });
 
 app.get('/Stratis', function(req, res){
-    // if (loggedIn)
-    //     res.render('StratisGit'); 
-    // else
-    //     res.render('login');
-    res.render('StratisGit'); 
+    if (req.session.key)
+        res.render('StratisGit'); 
+    else
+        res.render('login');
 });
 
 app.get('/PdfView', function(req, res){
-    // if (loggedIn)
-    //     res.render('PdfView'); 
-    // else
-    //     res.render('login');
-    res.render('PdfView'); 
+    if (req.session.key)
+        res.render('PdfView'); 
+    else
+        res.render('login');
 });
 
 app.get('/DomainResponse', function(req, res){
-    // if (loggedIn)
-    //     res.render('DomainResponse'); 
-    // else
-    //     res.render('login');
-    res.render('DomainResponse'); 
+    if (req.session.key)
+        res.render('DomainResponse'); 
+    else
+        res.render('login');
 });
 
 app.get('/Bithumb', function(req, res){
-    // if (loggedIn)
-    //     res.render('BithumbUrl'); 
-    // else
-    //     res.render('login');
-    res.render('BithumbUrl'); 
+    if (req.session.key)
+        res.render('BithumbUrl'); 
+    else
+        res.render('login');
 });
 
 app.get('/Top40', function(req, res){
-    // if (loggedIn)
-    //     res.render('Top40Url'); 
-    // else
-    //     res.render('login');
-    res.render('Top40Url'); 
+    if (req.session.key)
+        res.render('Top40Url'); 
+    else
+        res.render('login');
 });
 
 app.get('/CryptoWatch', function(req, res){
-    // if (loggedIn)
-    //     res.render('CryptoWatchUrl'); 
-    // else
-    //     res.render('login');
-    res.render('CryptoWatchUrl'); 
+    if (req.session.key)
+        res.render('CryptoWatchUrl'); 
+    else
+        res.render('login');
 });
 
 app.get('/Ens', function(req, res){
-    // if (loggedIn)
-    //     res.render('EnsUrl'); 
-    // else
-    //     res.render('login');
-     res.render('EnsUrl'); 
+    if (req.session.key)
+        res.render('EnsUrl'); 
+    else
+        res.render('login');
 });
 
 app.get('/CoinMarCap', function(req, res){
-    // if (loggedIn)
-    //     res.render('CoinMarCapUrl'); 
-    // else
-    //     res.render('login');
-    res.render('CoinMarCapUrl'); 
+    if (req.session.key)
+        res.render('CoinMarCapUrl'); 
+    else
+        res.render('login');
 });
 
 app.get('/ContackUs', function(req, res){
-    // if (loggedIn)
-    //     res.render('ContactUs'); 
-    // else
-    //     res.render('login');
-    res.render('ContactUs'); 
-    
+    if (req.session.key)
+        res.render('ContactUs'); 
+    else
+        res.render('login');
 });
 app.listen(process.env.PORT, process.env.IP);
 sys.puts('server running ' + 'now ' + Date.now());
